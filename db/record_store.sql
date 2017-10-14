39b4c624add3a2819cbdb97a4a2f91ec8fcee358
Peter McCready <petermccready2@gmail.com>
2017-10-14 01:51:12 +0100

    Adds record_store.sql that generates db tables

---
+++ db/record_store.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+
+CREATE TABLE albums(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  in_stock INT4,
+  stock_level VARCHAR(20)
+);
+
+
+CREATE TABLE artists(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  album_id INT8 REFERENCES albums(id) ON DELETE CASCADE
+);
