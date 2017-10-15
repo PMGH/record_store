f78bc9573eb9a1fd2b0eca561cea92d8dbde6a6f
Peter McCready <petermccready2@gmail.com>
2017-10-15 15:08:37 +0100

    Adds edit artist functionality

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -9,17 +9,25 @@ get '/big_als/artists/new' do
 end
 
 # edit artist
-# get '/big_als/artist/:id/edit' do
-#   erb( :"artists/edit" )
-# end
+get '/big_als/artists/:id/edit' do
+  @artist = Artist.find(params['id'])
+  erb( :"artists/edit" )
+end
 
 # show artist
 # get '/big_als/artist/:id' do
 #   erb( :"artists/show" )
 # end
 
+# create new artist and commit to db
 post '/big_als/artists/new' do
   artist = Artist.new(params)
   artist.save()
   erb( :"artists/create" )
+end
+
+post '/big_als/artists/:id/edit' do
+  artist = Artist.new(params)
+  artist.update()
+  erb ( :"artists/update" )
 end
