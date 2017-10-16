3fb645e173f3194ee7ca781f603170f4e16cce70
Peter McCready <petermccready2@gmail.com>
2017-10-16 09:55:38 +0100

    Adds delete artist functionality

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -3,21 +3,29 @@ require('sinatra/contrib/all')
 
 require_relative('../models/artist')
 
+# index
+# new
+# show
+# edit
+# create
+# update
+# delete
+
 # add new artist
 get '/big_als/artists/new' do
   erb( :"artists/new" )
-end
-
-# edit artist
-get '/big_als/artists/:id/edit' do
-  @artist = Artist.find(params['id'])
-  erb( :"artists/edit" )
 end
 
 # show artist
 # get '/big_als/artist/:id' do
 #   erb( :"artists/show" )
 # end
+
+# edit artist
+get '/big_als/artists/:id/edit' do
+  @artist = Artist.find(params['id'])
+  erb( :"artists/edit" )
+end
 
 # create new artist and commit to db
 post '/big_als/artists/new' do
@@ -31,4 +39,11 @@ post '/big_als/artists/:id' do
   artist = Artist.new(params)
   artist.update()
   erb ( :"artists/update" )
+end
+
+# delete existing artist
+post '/big_als/artists/:id/delete' do
+  artist = Artist.find(params['id'])
+  artist.delete()
+  erb ( :"artists/delete" )
 end
