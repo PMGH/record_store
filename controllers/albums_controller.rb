58e24894955abb8e464b5ffe5dc04b6b30c10308
Peter McCready <petermccready2@gmail.com>
2017-10-16 11:10:52 +0100

    Adds delete album functionality

--- controllers/albums_controller.rb
+++ controllers/albums_controller.rb
@@ -2,14 +2,39 @@ require('sinatra')
 require('sinatra/contrib/all')
 
 require_relative('../models/album')
+require_relative('../models/artist')
 
+# index
+# new
+# show
+# edit
+# create
+# update
+# delete
+
+# get new album form
 get '/big_als/albums/new' do
   @artists = Artist.all()
   erb( :"albums/new" )
 end
 
+# get edit album form
+get '/big_als/albums/:id/edit' do
+  @artists = Artist.all()
+  @album = Album.find(params['id'])
+  erb( :"albums/edit" )
+end
+
+# create album and save to db
 post '/big_als/albums/new' do
   album = Album.new(params)
   album.save()
   erb( :"albums/create" )
+end
+
+# update album
+post '/big_als/albums/:id' do
+  album = Album.new(params)
+  album.update()
+  erb( :"albums/update" )
 end
