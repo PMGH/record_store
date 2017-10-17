7277e36a4459255fe41dc2e2e7a8dec3588de483
Peter McCready <petermccready2@gmail.com>
2017-10-17 13:24:08 +0100

    Adds genre table

--- db/record_store.sql
+++ db/record_store.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS genres;
 DROP TABLE IF EXISTS artists;
 
 
@@ -9,12 +10,17 @@ CREATE TABLE artists(
   image TEXT
 );
 
+CREATE TABLE genres(
+  id SERIAL8 PRIMARY KEY,
+  genre VARCHAR(255)
+);
+
 CREATE TABLE albums(
   id SERIAL8 PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   in_stock INT4,
   stock_level VARCHAR(20),
   artist_id INT8 REFERENCES artists(id) ON DELETE CASCADE,
-  genre VARCHAR(255),
+  genre INT8 REFERENCES genres(id),
   artwork VARCHAR(255)
 );
